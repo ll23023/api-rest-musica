b26c4daf01787e650db127e7e64a78b33dc993a2
Karla <cg23041@ues.edu.sv>
2026-05-27 17:36:42 -0600

    Se agrega archivo service  y crontoller de albumes

---
+++ src/controller/album_controller.rs
@@ -0,0 +1,20 @@
+use crate::service::album_service::{
+    actualizar_album, agregar_album, eliminar_album, eliminar_album_por_id, obtener_album_por_id,
+    obtener_albumes,
+};
+use axum::{
+    Router,
+    routing::{delete, get, post, put},
+};
+use sqlx::PgPool;
+
+pub fn album_router(pool: PgPool) -> Router {
+    Router::new()
+        .route("/albumes", get(obtener_albumes))
+        .route("/albumes", post(agregar_album))
+        .route("/albumes", delete(eliminar_album))
+        .route("/albumes/:id", delete(eliminar_album_por_id))
+        .route("/albumes", put(actualizar_album))
+        .route("/albumes/:id", get(obtener_album_por_id))
+        .with_state(pool)
+}
---
+++ src/service/album_service.rs
@@ -0,0 +1,78 @@
+use crate::models::album::{ActualizarAlbum, Album, NuevoAlbum};
+use crate::repository::album_repository::AlbumRepository;
+use axum::{
+    Json,
+    extract::{Path, State},
+};
+use sqlx::PgPool;
+
+pub async fn obtener_albumes(State(pool): State<PgPool>) -> Json<Vec<Album>> {
+    let repo = AlbumRepository::new(pool);
+    match repo.obtener_albumes().await {
+        Ok(albumes) => Json(albumes),
+        Err(_) => Json(vec![]),
+    }
+}
+
+pub async fn obtener_album_por_id(
+    State(pool): State<PgPool>,
+    Path(id): Path<i32>,
+) -> Json<Option<Album>> {
+    let repo = AlbumRepository::new(pool);
+    match repo.obtener_album_por_id(id).await {
+        Ok(album) => Json(album),
+        Err(_) => Json(None),
+    }
+}
+
+pub async fn agregar_album(
+    State(pool): State<PgPool>,
+    Json(nuevo): Json<NuevoAlbum>,
+) -> Json<Option<Album>> {
+    let repo = AlbumRepository::new(pool);
+    match repo.agregar_album(nuevo).await {
+        Ok(album) => Json(Some(album)),
+        Err(_) => Json(Some(Album {
+            id_album: 0,
+            titulo: "Error al crear el álbum".to_string(),
+            fecha_lanzamiento: None,
+            id_artista: 0,
+        })),
+    }
+}
+
+pub async fn actualizar_album(
+    State(pool): State<PgPool>,
+    Path(id_album): Path<i32>,
+    Json(album_actualizado): Json<ActualizarAlbum>,
+) -> Json<Album> {
+    let repo = AlbumRepository::new(pool);
+    match repo.actualizar_album(id_album, album_actualizado).await {
+        Ok(album) => Json(album),
+        Err(_) => Json(Album {
+            id_album: 0,
+            titulo: "Error al actualizar el álbum".to_string(),
+            fecha_lanzamiento: None,
+            id_artista: 0,
+        }),
+    }
+}
+
+pub async fn eliminar_album(State(pool): State<PgPool>, Json(id_album): Json<i32>) -> Json<bool> {
+    let repo = AlbumRepository::new(pool);
+    match repo.eliminar_album(id_album).await {
+        Ok(_) => Json(true),
+        Err(_) => Json(false),
+    }
+}
+
+pub async fn eliminar_album_por_id(
+    State(pool): State<PgPool>,
+    Path(id_album): Path<i32>,
+) -> Json<bool> {
+    let repo = AlbumRepository::new(pool);
+    match repo.eliminar_album(id_album).await {
+        Ok(_) => Json(true),
+        Err(_) => Json(false),
+    }
+}
